20e5b87f56478a83c5f474d9240fb156a7027a25
Nainesh <ngoel@fastenal.com>
2023-06-04 00:58:25 +0530

    3 june TimeNeededToInformAllEmp

--- TimeNeededToInformAllEmployees.java
+++ TimeNeededToInformAllEmployees.java
@@ -1,26 +1,46 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TimeNeededToInformAllEmployees {
     public static void main(String[] args) {
 
-        int managers[] = { 2, 2, -1, 2, 2, 2 };
+        int managers[] = { 5,9,6,10,-1,8,9,1,9,3,4 };
         int n = managers.length;
-        int time[] = { 0, 0, 1, 0, 0, 0 };
+        int time[] = { 0,213,0,253,686,170,975,0,261,309,337 };
         Node nodes[] = new Node[n + 1];
         nodes[0] = new Node(-1, 0);
-
-        // id is true
-        // id=2, time[2], managers[2],
-        // but nodes[3]
-        //
-        for (int i = 0; i <= n; i++) {
-            if (managers[i - 1] == -1) {
-
-            } else {
-                nodes[i] = new Node(i - 1, time[i - 1]);
-            }
+        for(int i=1;i<=n;i++){
+            nodes[i] = new Node(i-1, time[i-1]);
         }
+        for(int i=1;i<=n;i++){
+            nodes[managers[i-1]+1].addNode(nodes[i]);
+        }
+
+        int ans=0;
+        Queue<Node> q=new LinkedList<>();
+        q.add(nodes[0]);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node temp= q.poll();
+                for(Node child: temp.children){
+                    if(child.time == 0){
+                        ans = Math.max(ans, temp.time);
+                    }else{
+                        child.time = child.time + temp.time;
+                        q.add(child);
+                    }
+                }   
+            }
+            // for(Node node: q) System.out.println(node.id);
+        }
+        
+        System.out.println(ans);
+        // System.out.println("--------------------------------------");
+        // for(Node node : nodes) System.out.println(node);
+        
     }
 
     static class Node {
@@ -37,6 +57,12 @@ public class TimeNeededToInformAllEmployees {
 
         public void addNode(Node n) {
             this.children.add(n);
+        }
+
+        public String toString(){
+            String kids="";
+            for(Node n : this.children) kids+=n.id+" ";
+            return this.id+" ("+this.time+") "+kids;
         }
     }
 }
---
+++ TimeNeededToInformAllEmployees2.java
@@ -0,0 +1,21 @@
+public class TimeNeededToInformAllEmployees2 {
+    //recursive solution
+    //using dp - dfs
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if (manager[i] != -1) {
+                result = Math.max(result, check(manager, informTime, i));
+            }
+        }
+        return result;
+    }
+
+    public int check(int[] manager, int[] informTime, int source) {
+        if (manager[source] != -1) {
+            informTime[source] = informTime[source] + check(manager, informTime, manager[source]);
+        }
+        manager[source] = -1;
+        return informTime[source];
+    }
+}
