15d8967472f682c311639c5c57158704ae45a779
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2025-11-01 13:36:07 +0530

    Time: 6 ms (15.45%), Space: 49.6 MB (6.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,39 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        //NOT OPTIMIZED, use BS to find the target instead of using Map\
+        //Map has complexity of O(n), in case of collision
+        // List<Integer> ans = new ArrayList<>();
+        // Map<Integer, Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(target-nums[i])){
+        //         ans.add(map.get(target- nums[i])+1);
+        //         ans.add(i+1);
+        //     }
+        //     map.put(nums[i], i);
+        // }
+        // return ans.stream().mapToInt(Integer::intValue).toArray();
+        
         List<Integer> ans = new ArrayList<>();
-        Map<Integer, Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                ans.add(map.get(target- nums[i])+1);
+            if(bs(nums, target-nums[i],i+1)!=-1 && bs(nums, target-nums[i],i+1)!=i){
                 ans.add(i+1);
+                ans.add(bs(nums, target-nums[i],i+1)+1);
+                break;
             }
-            map.put(nums[i], i);
         }
         return ans.stream().mapToInt(Integer::intValue).toArray();
- 
+       
     }
+    //bs optimize to first occurence
+     int bs(int nums[], int key, int lb){
+            int ub=nums.length-1;
+            while(lb<=ub){
+                int mid = lb + (ub-lb)/2;
+                if(nums[mid]==key) return mid;
+                else if(nums[mid]>key) ub=mid-1;
+                else lb=mid+1;
+            }
+            return -1;
+        }
 }
