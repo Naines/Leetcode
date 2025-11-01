e75a6ba54c7d59bcf571ceb9708a74d71c6bebee
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2025-11-01 14:22:16 +0530

    Time: 0 ms (100%), Space: 47.7 MB (5.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,8 @@
 class Solution {
     public double myPow(double x, int n) {
-       
        if(n==0) return 1;
+       //reduce n by 1 before converting to positibe, to fix overflow
+       // case: n = -2^31
        if(n<0) return 1/x * myPow(1/x, -(n + 1));
        double half= myPow(x*x, n/2);
         return n%2==0? half : x*half;
