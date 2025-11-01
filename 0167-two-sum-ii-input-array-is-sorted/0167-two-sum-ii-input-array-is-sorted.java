9af83458a358a497c43d4fef3a99e2b4f4a49e3c
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2025-11-01 13:46:02 +0530

    Time: 2 ms (93.38%), Space: 48.2 MB (6.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,20 @@
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
+        int left =0, right = nums.length-1;
+        while(left<right){
+            int sum = nums[left]+nums[right];
+            if(sum==target) return new int[]{left+1, right+1};
+            if(sum>target) right--;
+            else left++;
+        }
+        return new int[]{-1,-1};
+    }
+
+    //HASHMAP - not optimized - n
+    //BS dont use -nlogn
+    //use 2 pointers 
+    // public int[] twoSum(int[] nums, int target) {
 
         //NOT OPTIMIZED, use BS to find the target instead of using Map\
         //Map has complexity of O(n), in case of collision
@@ -14,26 +29,26 @@ class Solution {
         // }
         // return ans.stream().mapToInt(Integer::intValue).toArray();
         
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(bs(nums, target-nums[i],i+1)!=-1 && bs(nums, target-nums[i],i+1)!=i){
-                ans.add(i+1);
-                ans.add(bs(nums, target-nums[i],i+1)+1);
-                break;
-            }
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+    //     List<Integer> ans = new ArrayList<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         if(bs(nums, target-nums[i],i+1)!=-1 && bs(nums, target-nums[i],i+1)!=i){
+    //             ans.add(i+1);
+    //             ans.add(bs(nums, target-nums[i],i+1)+1);
+    //             break;
+    //         }
+    //     }
+    //     return ans.stream().mapToInt(Integer::intValue).toArray();
        
-    }
-    //bs optimize to not use first occurence
-     int bs(int nums[], int key, int lb){
-            int ub=nums.length-1;
-            while(lb<=ub){
-                int mid = lb + (ub-lb)/2;
-                if(nums[mid]==key) return mid;
-                else if(nums[mid]>key) ub=mid-1;
-                else lb=mid+1;
-            }
-            return -1;
-        }
+    // }
+    // //bs optimize to not use first occurence
+    //  int bs(int nums[], int key, int lb){
+    //         int ub=nums.length-1;
+    //         while(lb<=ub){
+    //             int mid = lb + (ub-lb)/2;
+    //             if(nums[mid]==key) return mid;
+    //             else if(nums[mid]>key) ub=mid-1;
+    //             else lb=mid+1;
+    //         }
+    //         return -1;
+    //     }
 }
