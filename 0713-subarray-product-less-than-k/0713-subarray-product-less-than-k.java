06be08c8f25d9f2abb351dea0d1b26e905a50012
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2025-12-11 19:01:45 +0530

    Time: 4 ms (99.81%), Space: 51.2 MB (34.29%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,32 +1,15 @@
 class Solution {
-
-     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k == 0) return 0;
-        int cnt = 0;
-        int pro = 1;
-        for (int i = 0, j = 0; j < nums.length; j++) {
-            pro *= nums[j];
-            while (i <= j && pro >= k) {
-                pro /= nums[i++];
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k==0) return 0;
+        int l=0, prd=1, ans=0;
+        for(int r=0;r<nums.length;r++){
+            prd*=nums[r];
+            //shrink window if prd>=k
+            while(prd>=k && l<=r){
+                prd/=nums[l++];
             }
-            cnt += j - i + 1;
+            ans+= r-l+1;
         }
-        return cnt;        
+        return ans;
     }
-    // public int numSubarrayProductLessThanK(int[] nums, int k) {
-    //     if(k==0) return 0;
-    //     int prd = 1;
-    //     int ans=0;
-    //     for(int j=0,start=0;j<nums.length;j++){
-    //         //expand
-    //         prd *=nums[j];
-    //         //shrink, decrease window by increasing left/start ptr
-    //         if(start<=j && prd>=k){
-    //             prd = prd/nums[start++];
-    //         }
-    //         System.out.println(start+" "+j);
-    //         ans+=j-start+1;
-    //     }
-    //     return ans;
-    // }
 }
