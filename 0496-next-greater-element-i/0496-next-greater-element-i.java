70541acd472d32315222f096b556b9958b7ecead
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2025-12-13 15:48:37 +0530

    Time: 7 ms (5.93%), Space: 45.3 MB (31.5%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums) {
-        List<Integer> ans =new ArrayList<>();
         List<Integer> res =new ArrayList<>();
         Stack<Integer> stk = new Stack<>();
+        Map<Integer, Integer> map =new HashMap<>();
         for(int i=nums.length-1;i>=0;i--){
             while (!stk.isEmpty() && stk.peek() <= nums[i])
                 stk.pop();
             if(stk.isEmpty()) {
-                ans.add(-1);
+                  map.put(nums[i], -1);
             }
             else { 
-                ans.add(stk.peek());
+                map.put(nums[i], stk.peek());
             }
             stk.push(nums[i]);
-        }
-        Collections.reverse(ans);
-        // for(int x: ans) System.out.println(x);
-        Map<Integer, Integer> map =new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i], ans.get(i));
         }
         for(int x: nums1){
            res.add(map.get(x));
