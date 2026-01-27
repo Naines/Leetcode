e35d027e85b6ec50b9dc9426b0721846f4dcc6c6
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-01-27 15:25:57 +0530

    Time: 101 ms (30.87%), Space: 48.8 MB (37.32%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,41 +1,5 @@
 class Solution {
     public int ladderLength(String start, String target, List<String> wordList) {
-        //  Set<String> dict=new HashSet<>();
-        // for(String s: wordList){
-        //     dict.add(s);
-        // }
-        // Deque<String> q=new ArrayDeque<>();
-        // q.addLast(start);
-        // int count=1;
-        
-        // while(!q.isEmpty())
-        // {
-        //     int size = q.size();
-        //     for(int i=0;i<size;i++)
-        //     {
-        //         char curr[] = q.poll().toCharArray();
-        //         for(int j=0;j<curr.length;j++)
-        //         {
-        //             char temp = curr[j];
-        //             for(char ch='a';ch<='z';ch++)
-        //             {
-        //                 curr[j] = ch;
-        //                 String next = new String(curr);
-        //                 if(dict.contains(next))
-        //                 {
-        //                     if(next.equals(target)) 
-        //                     return count+1;
-        //                     q.addLast(next);
-        //                     dict.remove(next);
-        //                 }
-        //             }
-        //             curr[j]=temp;
-        //         }
-        //     }
-        //     count++;
-        // }
-        // return 0;
-        
         Set<String> set = new HashSet<>(wordList);
         Deque<String> q=new ArrayDeque<>();
         q.add(start);
