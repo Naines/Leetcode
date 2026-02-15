6de0f3161598e88e731fad8357a3204a1a4f1285
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-02-16 03:47:08 +0530

    Time: 1 ms (83.6%), Space: 45 MB (22.35%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
        List<String> res = new ArrayList<>();
-        dfs(n,"", 0, 0 , res);
+        dfs(n,new StringBuilder(), 0, 0 , res);
         return res;
     }
 
-    void dfs(int n, String str, int o, int c, List<String> res){
-        if(str.length()==2*n){
-            res.add(str);
+    void dfs(int n, StringBuilder str, int o, int c, List<String> res){
+        if(str.toString().length()==2*n){
+            res.add(str.toString());
             return;
         }
 
-        if(o<n) dfs(n, str+"(", o+1, c, res);
-        if(c<o) dfs(n, str+")", o, c+1, res);
+        if(o<n) {dfs(n, str.append("("), o+1, c, res);
+         str.deleteCharAt(str.length() - 1);
+        }
+        if(c<o) {
+            dfs(n, str.append(")"), o, c+1, res);
+         str.deleteCharAt(str.length() - 1);
+        }
     }
 }
