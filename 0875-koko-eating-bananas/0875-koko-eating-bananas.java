4609b8ddce639eb14c609fc4a437a69abf027ede
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-02-21 16:10:09 +0530

    Time: 6 ms (99.62%), Space: 47.8 MB (87.14%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,37 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int n= piles.length;
-        Arrays.sort(piles);
-        int lb=1, ub=(int)1e9;
-        int ans = -1;
+        int max = 0;
+        for(int x: piles) max=Math.max(max, x);
+
+        int lb=1, ub=max;
+        int ans =-1;
         while(lb<=ub){
-            int mid = lb + (ub-lb)/2;
-            long tt=0;
-            for(int i=0;i<n;i++){
-                tt+= (piles[i]+mid-1)/mid;
-            }
-            if(tt>h){
-                lb = mid+1;
+            int m = lb+(ub-lb)/2; //current speed
+            if(check(piles,m,h)){
+                ans = m ;
+                ub=m-1;
             }else{
-                ub = mid-1;
-                ans=mid;
+                lb=m+1;
             }
-        } 
+        }
         return ans;
+    } 
+    boolean check(int arr[], int k, int h){
+        int n=arr.length;
+        int i=0;
+        for(i=0;i<n;i++){
+          int t= (arr[i]+k-1)/k;
+          h-=t;
+          if(h<0) break;
+        }
+        return i==n;
     }
-
-
-    //BRUTE FORCE 
-    // public int minEatingSpeed(int[] piles, int h) {
-    //     int k=1, n= piles.length;
-    //     Arrays.sort(piles);
-    //     while(true){
-    //         int tt=0;
-    //         for(int i=0;i<n;i++){
-    //             tt+=(piles[i]+k-1)/k;
-    //         }
-    //         if(tt<=h) return k;
-    //         k++;
-    //     }
-    // }
 }
