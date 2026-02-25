fd1dfc0f03281a4f01ca3ec49a9e835810284fdd
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-02-25 05:31:28 +0530

    Time: 2179 ms (5.01%), Space: 70.5 MB (8.76%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -5,14 +5,47 @@ class Solution {
         // for(int i=0;i<k-1;i++)pq.poll();
         // return pq.peek();
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int i=0;
-        for(i=0;i<k;i++) pq.add(nums[i]);
-        for(;i<nums.length;i++) 
-            if(nums[i]>pq.peek()) {
-                pq.poll();
-                pq.add(nums[i]);
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // int i=0;
+        // for(i=0;i<k;i++) pq.add(nums[i]);
+        // for(;i<nums.length;i++) 
+        //     if(nums[i]>pq.peek()) {
+        //         pq.poll();
+        //         pq.add(nums[i]);
+        //     }
+        // return pq.peek();
+
+        int l =0 , r= nums.length-1;
+        int n = nums.length;
+        Random rand = new Random();
+        while(true){
+            int partIdx = partition( nums, l, r, l+rand.nextInt(r-l+1));
+            if(partIdx==n-k) return nums[n-k];
+            else if(partIdx>n-k) r = partIdx-1;
+            else l = partIdx + 1;
+        } 
+    }
+
+    int partition(int nums[], int l, int r, int rand){
+        int pivot = nums[rand];
+        swap(nums, rand, r);
+        int i = l -1;
+        for(int j=l; j<=r-1;j++){
+            if(pivot> nums[j]){
+                i++;
+                swap(nums, i, j);
             }
-        return pq.peek();
+        }
+        swap(nums, i+1, r);
+        return i+1;
+    }
+   
+
+
+
+    void swap(int nums[], int l, int r){
+        int temp = nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
     }
 }
