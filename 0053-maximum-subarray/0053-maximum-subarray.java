b549a47165226754bc3b42830d0efd8a9f646856
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-02-27 04:45:05 +0530

    Time: 1 ms (99.73%), Space: 77.4 MB (47.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-      //5,4,-1,7,8
-      int sum = nums[0], maxSoFar = nums[0];
-      for(int i=1;i<nums.length;i++){
-        //get local maxima
-        sum = Math.max(sum+nums[i], nums[i]);
-
-        //get global maximum
-        maxSoFar = Math.max(maxSoFar, sum);
-      }
-
-      return maxSoFar;
+        int sum = nums[0], ans =sum;
+        for(int i=1;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum < nums[i]){
+                sum = nums[i] ;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
     }
 }
