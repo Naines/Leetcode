589fb15a4303a2ec4690dc7790d1611f6b1dcc1d
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-01 17:38:34 +0530

    Time: 5 ms (39.85%), Space: 45.6 MB (51.88%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -19,12 +19,6 @@ class Solution {
                 dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle.get(i-1).get(j-1);  
             }
         }
-        for(int i=0;i<=h;i++){
-            for(int j=0;j<=w;j++){
-                System.out.print(dp[i][j]+" ");
-            }
-            System.out.println();
-        }
         int ans = dp[h][1];
         for(int i=1;i<=w;i++){
             ans = Math.min(dp[h][i], ans);
