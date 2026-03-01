a55c13ad732126bc1fcf1734c3329b50b09f4c8e
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-01 15:22:17 +0530

    Time: 0 ms (100%), Space: 42.3 MB (27.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,20 @@
 class Solution {
-    
     public int uniquePaths(int m, int n) {
-         return solveHelper(m-1, n-1, new int[n][m]);  
-    }
+        int dp[][]=new int[m+1][n+1];
 
-    static int solveHelper(int m, int n, int memo[][] ){
-        if (m < 0 || n < 0) {
-            return 0;
-        } else if (m == 0 && n == 0) {
-            //destination
-            return 1;
-        } else if (memo[n][m] > 0) {
-            return memo[n][m];
-        }else {
-            memo[n][m] = solveHelper(m - 1, n, memo) + solveHelper(m, n - 1, memo);
-            return memo[n][m];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                if(j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
         }
+        return dp[m-1][n-1];
     }
 }
