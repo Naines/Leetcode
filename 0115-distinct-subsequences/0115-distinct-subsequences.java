5f1d33994b585fd7bc926c3dc9b21eb82da7da6f
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-02 13:53:38 +0530

    Time: 20 ms (53.97%), Space: 54.1 MB (77.52%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    //dp[i][j] -> number of subsequences of j in i
+    //if match -> dp[i-1][j]+dp[i-1][j-1]
+    //else dp[i-1][j]
+    //base case
+
+    
+    public int numDistinct(String s, String t) {
+        int m=s.length(),n=t.length();
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<=m;i++) dp[i][0]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==t.charAt(j-1))
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[m][n];
+    }
+}
