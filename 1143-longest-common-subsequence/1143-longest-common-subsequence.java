ffb22e33c8eec3e5a8bbc5f8eaf5e7b7bf989007
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-02 10:56:32 +0530

    Time: 20 ms (78.01%), Space: 53.8 MB (80.84%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -14,6 +14,25 @@ class Solution {
             }
             //System.out.println();
         }
+        //printlcs(dp, m , n, text1, text2);
         return dp[m][n];
+    }
+
+    void printlcs(int dp[][], int m , int n, String text1,String text2){
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                lcs.append(text1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        System.out.println(lcs.reverse().toString());
+        
     }
 }
