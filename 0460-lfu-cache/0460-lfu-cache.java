e0d5c7d9f68f4f30f5b099c77d66d35dbe020061
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-04 10:08:07 +0530

    Time: 65 ms (45.81%), Space: 143.2 MB (14.18%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -17,28 +17,44 @@ class LFUCache {
             return -1;
         int count = counts.get(key);
         counts.put(key, count+1);
+
+        //remove and add
         lists.get(count).remove(key);
+
+        //min determines the lfu
         if(count==min && lists.get(count).size()==0)
             min++;
         if(!lists.containsKey(count+1))
             lists.put(count+1, new LinkedHashSet<>());
         lists.get(count+1).add(key);
+
+
         return vals.get(key);
     }
     
     public void put(int key, int value) {
+        //no capacity to put
         if(cap<=0)
             return;
+
+        //cache has key, then increase count and rearrange in appropriate list
         if(vals.containsKey(key)) {
             vals.put(key, value);
             get(key);
             return;
         } 
+
+        //cache eviction
         if(vals.size() >= cap) {
+
+            //get min key from list with min count,
+            //remove from list and values map
             int evit = lists.get(min).iterator().next();
             lists.get(min).remove(evit);
-            vals.remove(evit);
+            vals.remove(evit);counts.remove(evit);
         }
+
+        //
         vals.put(key, value);
         counts.put(key, 1);
         min = 1;
