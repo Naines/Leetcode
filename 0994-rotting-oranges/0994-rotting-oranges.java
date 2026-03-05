b2f1525287ac5010a8bf0e3d0313a691258a4572
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-05 14:27:35 +0530

    Time: 4 ms (6.37%), Space: 44.1 MB (84.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,38 +1,47 @@
 class Solution {
-    static int dirs[][]={{1,0},{-1,0},{0, 1},{0,-1}};
+    //add rotten to queue
+    //every second pop all from queue and add neighbours to queue
+    //at end, check if there are fresh oranges left
+    //if cnt>0 return -1
+
     public int orangesRotting(int[][] grid) {
-        int h = grid.length, w= grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        int cf=0, ans=0;
+        int cnt = 0, time=0;
+        int h=grid.length, w=grid[0].length;
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<h;i++){
             for(int j=0;j<w;j++){
-                if(grid[i][j]==2)
-                    q.add(new int[]{i, j});
-                else if(grid[i][j]==1)
-                    cf++;
+                if(grid[i][j]==1) cnt++;
+                else if(grid[i][j]==2) q.add(new int[]{i, j});
             }
         }
-       
-        if(cf==0) return 0;
-        
-        while(!q.isEmpty()){
-            ans++;
-            int size= q.size();
-            while(size-->0){
-                int t[]= q.poll();
-                for(int dir[]: dirs){
-                    int x = t[0]+ dir[0];
-                    int y = t[1]+ dir[1];
-                    if(x < 0 || y < 0 || x >= h|| y >=w || grid[x][y] == 0 ||
-                    grid[x][y] == 2)
-                        continue;
-                    grid[x][y]=2;
-                    q.add(new int[]{x,y});
-                    cf--;
+        // 2,1,1
+        // 1,1,0
+        // 0,1,1
+
+        // 2,1,1
+        // 0,1,1
+        // 1,0,1
+        if(cnt==0) return 0;
+        int dir[][]= {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(q.size()>0){
+            int size = q.size();
+            for(int j=0;j<size;j++){
+                int t[]=q.poll();
+                //System.out.println(t[0]+" "+t[1]+" "+grid[t[0]][t[1]]);
+                for(int i=0;i<4;i++){
+                    int cr = t[0]+dir[i][0];
+                    int cc= t[1]+dir[i][1];
+                    if(cr>=0 && cc>=0 && cr<h && cc<w && grid[cr][cc]==1){
+                        cnt--;
+                        q.add(new int[]{cr, cc});
+                        grid[cr][cc]=2;
+                    }
                 }
             }
+            //System.out.println("time = "+time);
+            time++;
         }
-        return cf>0?-1:ans-1; 
-
+        return cnt>0?-1:time-1;
     }
 }
