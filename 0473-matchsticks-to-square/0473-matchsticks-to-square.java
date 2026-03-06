6024f89cd5cdba304bec653b79fe0369fcb6c484
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-06 20:42:29 +0530

    Time: 133 ms (27.56%), Space: 43.5 MB (23.07%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -0,0 +1,28 @@
+class Solution {
+    int arr[];
+    int len, trgt;
+    public boolean makesquare(int[] matchsticks) {
+        int sum = Arrays.stream(matchsticks).sum();
+        if(sum%4!=0){
+            return false;
+        }
+        this.arr = matchsticks;
+        this.len = matchsticks.length;
+        this.trgt = sum/4;
+        return dfs(0, 0, 4, new boolean[len]);//currSum, currIdx, k
+    }
+
+    boolean dfs(int currSum, int idx, int k, boolean vis[]){
+        if(k==0) return true;
+        if(currSum==trgt) return dfs(0, 0, k-1, vis);
+        for(int i=idx;i<len;i++){
+            if(vis[i]) continue;
+            if(arr[i]+currSum> trgt) continue;
+
+            vis[i]=true;
+            if(dfs(currSum+arr[i], i+1, k, vis)) return true;
+            vis[i]=false;
+        }
+        return false;
+    }
+}
