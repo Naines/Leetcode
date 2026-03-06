25d50737409d52f8b76028de1278a1cbceaf8e42
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-06 21:20:43 +0530

    Time: 124 ms (6.19%), Space: 65.3 MB (74.57%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,33 +1,55 @@
 class Solution {
+
+    //aaabb
+    //left
+
+    //substring palindrome is required and not subsequence
+    //hence string.substring van be used.
+    //if left is palin, dfs on (right)
+
+    //a -> (aabb) -> a -> (abb) -> a -> bb -> b -> b
+    //aa -> abb 
+    //aab -> bb
+    
+
+    //need to use map to save TLE
+
+    Map<String , Boolean> map =new HashMap<>();
+    List<List<String>> ans = new ArrayList<>();
     public List<List<String>> partition(String s) {
-         List<List<String>> res = new ArrayList<>();
-         dfs(res, new ArrayList<String>(), s);
-         return res;
+        dfs(new ArrayList<>(), s);
+        return ans;
     }
 
-    void dfs( List<List<String>> res, List<String> curr, String s){
-        if(s.length()==0){
-            res.add(new ArrayList<String>(curr));
+    void dfs(ArrayList<String> list,String s){
+        if(0==s.length()){
+            ans.add(new ArrayList<>(list));
             return;
         }
+        System.out.println("IN AT"+s);
         for(int i=0;i<s.length();i++){
-            String left=s.substring(0, i+1);
+            String left = s.substring(0, i+1);
             if(isPalin(left)){
-                curr.add(left);
-                String right=s.substring(i+1);
-                dfs(res, curr, right);
-                curr.remove(curr.size()-1);
+                list.add(left);
+                
+                String right = s.substring(i+1);
+                dfs(list, right);
+                list.remove(list.size()-1);
+              
             }
         }
+        // map.put(s, false);
+        return;
     }
 
-    boolean isPalin(String str){
-        int l=0, r=str.length()-1;
+    boolean isPalin(String word){
+        int l=0, r=word.length()-1;
         while(l<r){
-            if(str.charAt(l)==str.charAt(r)){
-                l++;
-                r--;
-            }else return false;
+            if(word.charAt(l)==word.charAt(r)){
+                l++;r--;
+            }else{
+                return false;
+            }
         }
         return true;
     }
