c976d501e13d35a5d437594a3cf1c36f85f6a158
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-08 18:07:32 +0530

    Time: 1 ms (26.14%), Space: 45.5 MB (78.4%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,21 +16,34 @@
 class Solution {
     Map<TreeNode , Integer> map =new HashMap<>();
     public boolean isBalanced(TreeNode root) {
-        return dfs(root); 
+        // return dfs(root); 
+        return depth(root)!=-1;
     }
 
-    boolean dfs(TreeNode root){
-        if(root==null) return true;
-        int left = height(root.left);
-        int right = height(root.right);
-        if(Math.abs(left-right)<=1 && dfs(root.left) && dfs(root.right)) return true;
-        return false;
-    }
-
-    int height(TreeNode root){
+    int depth(TreeNode root){
         if(root==null) return 0;
-        if(map.get(root)!=null) return map.get(root);
-        map.put(root, Math.max(height(root.left), height(root.right))+1);
-        return map.get(root);
+        int left = depth(root.left);
+        int right = depth(root.right);
+
+        if(left==-1) return -1;
+        if(right==-1) return -1;
+
+        if(Math.abs(left-right)>1) return -1;
+        return Math.max(left, right)+1;
     }
+
+    // boolean dfs(TreeNode root){
+    //     if(root==null) return true;
+    //     int left = height(root.left);
+    //     int right = height(root.right);
+    //     if(Math.abs(left-right)<=1 && dfs(root.left) && dfs(root.right)) return true;
+    //     return false;
+    // }
+
+    // int height(TreeNode root){
+    //     if(root==null) return 0;
+    //     if(map.get(root)!=null) return map.get(root);
+    //     map.put(root, Math.max(height(root.left), height(root.right))+1);
+    //     return map.get(root);
+    // }
 }
