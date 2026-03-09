23e3dd8f4e29aab4e813fe783a8ee69bbd0a69a2
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-09 20:07:54 +0530

    Time: 44 ms (84.27%), Space: 131.1 MB (69.4%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,86 +1,91 @@
 class LRUCache {
-    Map<Integer, Node> map;
-    int capacity;
+    static class Node{
+        int k;
+        int v;
+        Node prev, next;
+        Node(){
+            // this.v = ;
+            // this.k = -1;
+        }
+        Node(int k, int v){
+            this.k=k;
+            this.v= v;
+        }
+    }
+    int capacity = 0, count = 0;
+    Map<Integer, Node> map= new HashMap<>();
     Node head, tail;
-    int count;
-    
     public LRUCache(int capacity) {
-       this.capacity = capacity;
-       this.count =0;
-       map = new HashMap<>();
-       head = new Node();
-       tail = new Node();
-       head.next = tail;
-       tail.prev= head;
+        this.capacity = capacity;
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev= head;
     }
     
     public int get(int key) {
+        
+        if(!map.containsKey(key))
+        return -1;
         Node n = map.get(key);
-        if(n == null){
-            return -1;
-        }
         update(n);
         return n.v;
     }
     
-    public void put(int k, int v) {
-       Node n = map.get(k);
-       if(n==null){
-        //create and add to list and map
-        n = new Node(k, v);
-        map.put(k, n);
-        add(n);
-        count++;
-       }else{
-        //update
-        n.v = v;
-        update(n);
-       }
-        //reset
-       if(count > capacity){
-        Node toDel = tail.prev;
-        remove(toDel);
-        map.remove(toDel.k);
-        count--;
-       }
+    public void put(int key, int value) {
+        if(!map.containsKey(key)){
+            Node n = new Node(key, value);
+            map.put(key, n);
+            add(n);
+            count++;
+        
+        }else{
+            Node n = map.get(key);
+            n.v = value;
+            update(n);
+        }
+
+        //delete least recently used in case of overflow
+        if(count > capacity){
+            Node toDel = tail.prev;
+            remove(toDel);
+            map.remove(toDel.k);
+            count--;
+        }
+        // print(head);
+        // System.out.println("Size="+map.size());
+        return;
     }
 
-    private void update(Node n){
+
+    //////////////////////////////////////
+    void update(Node n){
+        
         remove(n);
         add(n);
     }
 
-    //add after head node
-    private void add(Node n){
-        Node after = head.next;
-        head.next = n;
-        n.prev = head;
-
-        n.next = after;
-        after.prev= n;
-
+    void add(Node n){  
+        Node t = head.next;
+        head.next=n;
+        n.next=t;
+        n.prev= head;
+        t.prev=n;
     }
 
-    //remove node before tail
-    private void remove(Node n){
-        Node before = n.prev, after = n.next;
-        before.next = after;
-        after.prev= before;
+    void remove(Node n){
+        Node t = n.prev, after = n.next;
+        t.next  = after;
+        after.prev = t;
     }
 
-
-     static class Node{
-        int k, v;
-        Node prev, next;
-        Node(int k, int v){
-            this.k = k;
-            this.v =v;
-            
+    void print( Node head){
+        Node t = head;
+        while(t!=null){
+            System.out.print(t.v+"-");
+            t=t.next;
         }
-        Node(){
-            System.out.println("Node init");
-            this(0,0);
-        }
+        System.out.println();
     }
 }
 
