ef252153e5cd8d9a05d2ba193899c4a5fd25a44e
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-11 13:46:49 +0530

    Time: 16 ms (98.29%), Space: 46.9 MB (67.97%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.java
+++ 1117-building-h2o/1117-building-h2o.java
@@ -1,22 +1,32 @@
 class H2O {
 
-    Semaphore h1,h2,o;
-    public H2O() {
-        h1=new Semaphore(2);
-        o=new Semaphore(0);
+    private Semaphore h = new Semaphore(2);
+    private Semaphore o = new Semaphore(1);
+    private Semaphore d = new Semaphore(0);
+    public H2O() {    
     }
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-		h1.acquire();
-        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line
+        // do hydrogen if allowed
+        h.acquire();
         releaseHydrogen.run();
-        o.release();
+        d.release();
     }
 
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        o.acquire(2);
+        
         // releaseOxygen.run() outputs "O". Do not change or remove this line.
+
+        // do oxygen if allowed
+        o.acquire();
 		releaseOxygen.run();
-        h1.release(2);
+        // wait till hydrogen is all done
+        d.acquire();
+        d.acquire();
+
+        //reset for next
+        h.release(2);
+        o.release();
     }
 }
