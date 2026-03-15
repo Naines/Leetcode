62c7a701ec83d1233d3151ba25f9d1b7ea7573a8
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-16 03:33:18 +0530

    Time: 21 ms (33.61%), Space: 48 MB (40.26%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,29 +1,26 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int max = 0;
-        for(int x: piles) max=Math.max(max, x);
-
-        int lb=1, ub=max;
-        int ans =-1;
+        Arrays.sort(piles);
+        int lb=1;
+        int ub = Arrays.stream(piles).max().getAsInt();
+        int ans =0;
+        //System.out.println(lb+" "+ub);
         while(lb<=ub){
-            int m = lb+(ub-lb)/2; //current speed
-            if(check(piles,m,h)){
-                ans = m ;
-                ub=m-1;
+            int m = lb + (ub-lb)/2;
+            long hours = 0;
+            for(int i=0;i<piles.length;i++){
+                hours+= (piles[i]+m-1)/m;
+            }
+            //1,2,3,.....11
+            //5
+            if(hours<=h){
+                ans = m;
+                ub=m-1; 
             }else{
                 lb=m+1;
             }
-        }
+        }   
         return ans;
-    } 
-    boolean check(int arr[], int k, int h){
-        int n=arr.length;
-        int i=0;
-        for(i=0;i<n;i++){
-          int t= (arr[i]+k-1)/k;
-          h-=t;
-          if(h<0) break;
-        }
-        return i==n;
     }
+    
 }
