2c59414948f97ae828dab1b68564ffa4566ec116
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-24 04:05:49 +0530

    Time: 3 ms (42.84%), Space: 88.1 MB (68.7%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -1,14 +1,42 @@
 class Solution {
-    public long countSubarrays(int[] nums, long k) {
-        int l=0,r=0;
-        long ans =0 , currSum = 0;
-        for(r=0;r<nums.length;r++){
-            currSum+=nums[r];
-            while(currSum*(r-l+1)>=k){
-                currSum-=nums[l++];
+    //score<k
+    //a>0
+    //   l....r -> score<k
+    // count[]
+     public long countSubarrays(int[] arr, long k) {
+        int n = arr.length,l=0;
+        long ans = 0;
+        long sum = 0;
+        for (int r = 0; r < n; r++) {
+            sum+=arr[r];
+            while(!isValid(r-l+1, sum, k)){
+                sum-=arr[l];
+                l++;
             }
             ans+=r-l+1;
         }
         return ans;
     }
+    boolean isValid(int len, long sum, long k){
+        long mul = len*sum;
+        return mul<k;
+    }
+    // public int countSubarrays(int[] arr, long k) {
+    //     int n = arr.length;
+    //     int ans = 0;
+
+    //     for (int i = 0; i < n; i++) {
+    //         int sum = 0;
+    //         for (int j = i; j < n; j++) {
+    //             sum += arr[j];
+    //             int length = j - i + 1;
+    //             long score = (long) sum * length;
+    //             if (score < k) {
+    //                 //System.out.println(score+" "+j+" "+i);
+    //                 ans++;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 }
