5b6ae193e99a57852cf8a995b4e9fddbc6d3f4dc
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-25 21:47:20 +0530

    Time: 3 ms (75.22%), Space: 45.4 MB (92.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,24 +1,23 @@
 class Solution {
-    public String minWindow(String str, String ttr) {
-        int l=0,n1=str.length(), n2=ttr.length(), ans=Integer.MAX_VALUE, cnt=0, startIndex=-1;
-        char s[]=str.toCharArray(), t[]=ttr.toCharArray();
+    public String minWindow(String s, String t) {
+        int n1=s.length(),n2=t.length(),ans=Integer.MAX_VALUE,cnt=0,start=-1,l=0;
         int map[]=new int[256];
-        for(char c:t) map[c]++;
-        for(int r=0;r<n1;r++){
-            if(map[s[r]]>0) cnt++;
-            map[s[r]]--;
-            //System.out.println(r+" "+l+" "+s[r]+" "+cnt);
-            while(cnt==t.length){
+        for(char c: t.toCharArray()) map[c]++;
+        for(int r=0;r<s.length();r++){
+            char ch=s.charAt(r);
+            if(map[ch]>0) cnt++;
+            map[ch]--;
+            while(cnt==t.length()){
                 if((r-l+1)<ans){
                     ans = r-l+1;
-                    startIndex = l;
+                    start=l;
                 }
-                map[s[l]]++;
-                if(map[s[l]]>0) cnt--;
+                map[s.charAt(l)]++;
+                if(map[s.charAt(l)]>0) cnt--;
                 l++;
             }
-        }
-        //System.out.println(startIndex+" "+ans);
-        return startIndex==-1?"":str.substring(startIndex, startIndex+ans);
+        } 
+        //System.out.println(start+" "+ans);
+        return start==-1?"":s.substring(start,start+ans);
     }
 }
