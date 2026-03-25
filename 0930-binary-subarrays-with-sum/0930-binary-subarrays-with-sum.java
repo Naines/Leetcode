74d708c2a7ca95a2df1dd99f9f64c1a74e9b5e21
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-25 15:28:19 +0530

    Time: 22 ms (43.59%), Space: 50.2 MB (97.16%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,4 +1,22 @@
 class Solution {
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int pre=0, ans =0;
+        HashMap<Integer, Integer> map=new HashMap<>();
+        map.put(0, 1);
+
+        for(int r=0;r<nums.length;r++){
+            pre+=nums[r];
+            ans+=map.getOrDefault(pre-goal,0);
+            map.put(pre, map.getOrDefault(pre, 0)+1);
+            
+        }
+        
+        return ans;
+    }
+
+
+    //M1: sliding window <=goal
     //rrrrr
     //l
     //00000 1+2+3+4+5
@@ -11,24 +29,24 @@ class Solution {
     //window=should have sum goal
 
     //ans = 1+1
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        System.out.println(countlessThanGoal(nums, goal));
-        System.out.println(countlessThanGoal(nums, goal-1));
+    // public int numSubarraysWithSum(int[] nums, int goal) {
+    //     System.out.println(countlessThanGoal(nums, goal));
+    //     System.out.println(countlessThanGoal(nums, goal-1));
 
-      return countlessThanGoal(nums, goal)-countlessThanGoal(nums, goal-1);
-    }
+    //   return countlessThanGoal(nums, goal)-countlessThanGoal(nums, goal-1);
+    // }
 
-    int countlessThanGoal(int nums[], int goal){
-        if(goal<0) return 0;
-        int l=0, ans = 0, sum=0;
-        for(int r=0;r<nums.length;r++){
-            sum+=nums[r];
-            while(sum>goal){
-                sum-=nums[l];
-                l++;
-            }
-            ans+=r-l+1; //(all subarray ending at r)
-        }
-        return ans;
-    }
+    // int countlessThanGoal(int nums[], int goal){
+    //     if(goal<0) return 0;
+    //     int l=0, ans = 0, sum=0;
+    //     for(int r=0;r<nums.length;r++){
+    //         sum+=nums[r];
+    //         while(sum>goal){
+    //             sum-=nums[l];
+    //             l++;
+    //         }
+    //         ans+=r-l+1; //(all subarray ending at r)
+    //     }
+    //     return ans;
+    // }
 }
