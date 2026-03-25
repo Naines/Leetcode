129fb54477542f181a3b8d1a09495d6f471f3fb0
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-03-25 15:52:38 +0530

    Time: 49 ms (7.34%), Space: 56.8 MB (76.77%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,32 +1,21 @@
 class Solution {
+    //rrrrrr
+    //211211,3
+
+    //21121
+    //1121
+    //1211
+
+    //if(count==k) ans++;
     public int numberOfSubarrays(int[] nums, int k) {
-        // Map<Integer, Integer> map=new HashMap<>();
-        // map.put(0, 1);
-        // int l=0,n=nums.length, nodd=0;
-        // int count=0;
-        // for(int r=0;r<n;r++){
-        //     if((nums[r]&1)==1) nodd++;
-        //     count+=map.getOrDefault(nodd-k,0);
-        //     map.put(nodd, map.getOrDefault(nodd,0)+1);
-        // }
-        // return count;
-
-        return atmost(nums, k) - atmost(nums, k-1);
-    }
-
-    int atmost(int nums[], int k){
-        if(k<0) return 0;
-        int l=0, nodd=0, count=0;
-        for(int r=0;r<nums.length;r++){
-            if((nums[r]&1)==1) nodd++;
-            while(nodd>k){
-               if(nums[l]%2!=0){
-                nodd--;
-               }
-               l++;
-            }
-            count+=r-l+1;
+        int pre=0, n=nums.length, ans=0;
+        Map<Integer, Integer> map=new HashMap<>();
+        map.put(0, 1);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2!=0) pre++;
+            ans+=map.getOrDefault(pre-k,0);
+            map.put(pre, map.getOrDefault(pre,0)+1);
         }
-        return count;
+        return ans;
     }
 }
