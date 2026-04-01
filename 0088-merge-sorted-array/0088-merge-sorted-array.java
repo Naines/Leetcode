d0604d93c68383aa3807f2e59951c6b819e6cbfe
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-04-01 13:23:29 +0530

    Time: 0 ms (100%), Space: 43.7 MB (69.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int l = m+n;
-        int ans[]=new int[l];
-        int i1=0, i2=0,k=0;
-        while(i1<m && i2<n){
-            if(nums1[i1]<=nums2[i2]){
-                ans[k++]=nums1[i1++];
+        //copy from behind
+        int k = m+n-1;
+        n=n-1;
+        m=m-1;
+        while(n>=0) {
+            if(m>=0 && nums1[m]>nums2[n]) {
+                nums1[k--]=nums1[m--];
             }else{
-                ans[k++]=nums2[i2++];
+                nums1[k--] = nums2[n--];
             }
         }
-        while(i1<m) ans[k++]=nums1[i1++];
-        while(i2<n) ans[k++]=nums2[i2++];
-        for(int i=0;i<l;i++){
-            nums1[i]=ans[i];
-        }
+
+        // int l = m+n;
+        // int ans[]=new int[l];
+        // int i1=0, i2=0,k=0;
+        // while(i1<m && i2<n){
+        //     if(nums1[i1]<=nums2[i2]){
+        //         ans[k++]=nums1[i1++];
+        //     }else{
+        //         ans[k++]=nums2[i2++];
+        //     }
+        // }
+        // while(i1<m) ans[k++]=nums1[i1++];
+        // while(i2<n) ans[k++]=nums2[i2++];
+        // for(int i=0;i<l;i++){
+        //     nums1[i]=ans[i];
+        // }
     }
 }
