89137c4f7b37ced6187ecd3d19d913d7e59a4e93
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-04-06 15:33:16 +0530

    Time: 37 ms (85%), Space: 53.9 MB (75.62%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,21 +1,37 @@
 class Solution {
+    int m=(int)1e9+7;
     public int sumSubarrayMins(int[] arr) {
         int n =arr.length;
         long sum=0;
-        int m=(int)1e9+7;
-        int j,k;
-        int dp[]=new int[n+1];
+       
         Stack<Integer> stk=new Stack<>();
-        stk.push(-1);
+        int nse[]=new int[n];
+        int pse[]=new int[n];
+        Arrays.fill(nse, -1);
+        Arrays.fill(pse, -1);
         for(int i=0;i<arr.length;i++){
-            while(stk.peek()!=-1 && arr[stk.peek()]>=arr[i]){
+            while(!stk.isEmpty() && arr[stk.peek()]>=arr[i]){
                 int t = stk.pop();
+                nse[t]=i;
             }
-            dp[i+1] = (dp[stk.peek()+1] + (i-stk.peek()) *arr[i]) %m;
+            if(!stk.isEmpty()){
+                pse[i]= stk.peek();
+            }
             stk.push(i);
-            sum+=dp[i+1];
-            sum%=m;
+        }
+        for(int i=0;i<n;i++){
+            int ld = pse[i]==-1?i+1:(i-pse[i]);
+            int rd = nse[i]==-1?n-i:(nse[i]-i);
+            // System.out.println(arr[i]+" "+ld+" "+rd);
+            sum = add(sum , mul(arr[i], mul(ld, rd)) );
         }
         return (int)sum;
+    }
+
+    long add(long a, long b){
+        return (a+b)%m;
+    }
+    long mul(long a, long b) {
+        return (a*b)%m;
     }
 }
