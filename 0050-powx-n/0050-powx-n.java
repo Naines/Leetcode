c31bb2cf700c7c62a8c3dc9b6c1a9e0f67aba484
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-04-18 20:13:12 +0530

    Time: 0 ms (100%), Space: 47.8 MB (73.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,17 +1,24 @@
 class Solution {
+    //x^n
+    //if(n<0) return 1/pow(x, n)
+    //else if (n>0) return pow(x,n)
+    //else return 1;
+    double res = 1.0;
     public double myPow(double x, int n) {
-        if(n<0) {
-            double res =  1/solve(x, n*-1);
-            return res;
-        }
-        else return solve(x, n);
+        if(x==1) return 1.0d;
+        if(n<0) return 1/pow(x, -n);
+        else if(n>0) return pow(x, n);
+        else return 1.0d;
     }
 
-    static double solve(double x, int n){
-        if(n==0) return 1;
-        double half = solve(x, n/2);
-        if(n%2==0) return half*half;
+    //10^9~2^30
+    //pow(2, 5) -> pow(2, 2)-> pow(2,1)
+    double pow(double x,int n){
+       if(n==0) return 1;
+        double half = pow(x, n/2);
+        if(n%2==0){
+            return half*half;
+        }
         return half*half*x;
     }
-    
 }
