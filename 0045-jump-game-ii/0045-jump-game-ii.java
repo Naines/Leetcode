9647ac6bf72cc7ce16051b4583d70c5f5eac6d82
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-04-23 03:00:25 +0530

    Time: 1 ms (99.55%), Space: 47.2 MB (58.27%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -7,17 +7,14 @@ class Solution {
      f[n]=Math.min(f[1]....f[n-1])+1
      */
     public int jump(int[] nums) {
-        int n=nums.length;
-        int dp[] = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[1] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i - 1; j++) {
-                if (nums[j - 1] + j >= i) {
-                    dp[i] = Math.min(dp[i], dp[j] + 1);
-                }
+        int jumps = 0, curEnd = 0, currMax = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            currMax = Math.max(currMax, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = currMax;
             }
         }
-        return dp[n];
+        return jumps;
     }
 }
