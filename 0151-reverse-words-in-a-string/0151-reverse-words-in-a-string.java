f6709af86da1fe7f25545a1fe7388eb220fb6b20
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-04-26 19:54:39 +0530

    Time: 7 ms (65.35%), Space: 45.1 MB (23.69%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=words.length-1;i>=0;i--){
             String cword=words[i].trim();
             if(hasNoWhiteSpace(cword) && cword.length()>0) {
-                System.out.println(cword+".");
+                // System.out.println(cword+".");
 
                 sb.append(cword).append(" ");
             }
