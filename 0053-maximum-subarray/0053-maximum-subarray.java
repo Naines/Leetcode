f4922f2192bf4145978520a67e2e67ade7d6cef0
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-03 00:31:16 +0530

    Time: 1 ms (99.97%), Space: 77.5 MB (14.86%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,31 +1,14 @@
 class Solution {
-
-    //find the range sum, if range sum<maxSum, reset range start
-    //maxSum = -Infinity
-
-
-    //-2,1,-3,4,-1,2,1,-5,4
-    // -2, -1, -4,
-    // 4 -1 
+    /** 
+    prefix max So Far
+     */
     public int maxSubArray(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int maxEnd=nums[0], maxSum= nums[0];
-        int cs = 0, s=0, e=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]+maxEnd < nums[i]){
-                maxEnd = nums[i];
-                cs = i;
-            }else{
-                maxEnd = nums[i]+maxEnd;
-            }
-            
-            if(maxEnd > maxSum){
-                maxSum = maxEnd;
-                s=cs;
-                e=i;
-            }
+        int ans =Integer.MIN_VALUE, sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            sum=Math.max(sum, nums[i]);
+            ans=Math.max(ans, sum);
         }
-        //System.out.println(s+" "+e);
-        return maxSum;
+        return ans;
     }
 }
