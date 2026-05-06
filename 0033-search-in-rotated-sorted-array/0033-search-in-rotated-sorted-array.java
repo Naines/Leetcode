7081a6d046d7263bef831c86a514c83c1261556a
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-06 23:19:28 +0530

    Time: 0 ms (100%), Space: 44 MB (26.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,21 @@
 class Solution {
+
+    //4,5,6,7,0,1,2   0
+    //f f f f t t t
+    //4,5,6,7,0,1,2   3
     public int search(int[] nums, int target) {
-        int lb = 0, ub = nums.length-1;
-        while(lb<=ub){
-            int m = (lb+ub)/2;
-            if(nums[m]==target){
-                return m;
-            }
+        int lb=0, ub=nums.length-1;
+       while(lb<=ub){
+            int m = (lb+ub)>>1;
+            if(nums[m]==target) return m;
+            //left side sorted?
             if(nums[lb]<=nums[m]){
-
-                if(nums[lb]<=target && target<=nums[m]){
-                    ub = m-1;
-                }else{
+                //if sorted, then target lies within [lb..m]?
+                if(target>=nums[lb] && target<=nums[m])
+                    ub=m-1;
+                else
                     lb=m+1;
-                }
-
-            }else{
-                
+            }else{ 
                 if(target>=nums[m] && target<=nums[ub]){
                     lb=m+1;
                 }else{
