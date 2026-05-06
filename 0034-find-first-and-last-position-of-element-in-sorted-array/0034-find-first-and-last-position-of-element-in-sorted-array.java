04cfe72870da14b0130be65d3cce2cec1f596853
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-06 23:09:14 +0530

    Time: 0 ms (100%), Space: 48.3 MB (30.85%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,43 +4,23 @@ class Solution {
     //0 1 2 3 4 5
     //f t t f f f      
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length==0) return new int[]{-1,-1};
-        int first = lower(nums, target);
-        int last = upper(nums, target);
-
-        first = nums[first]==target?first:-1;
-        last = nums[last]==target?last:-1;
-        return new int[]{first, last};
+        int ans = lower(nums, target);
+        if(ans == nums.length || nums[ans] !=target){
+            return new int[]{-1,-1};
+        }
+        return new int[]{ans, lower(nums, target+1)-1};
     }
-
 
     int lower(int nums[], int target){
-        int lb=0, ub=nums.length-1;
-        int ans=0;
-        while(lb<=ub){
+        int lb=0, ub=nums.length;
+        while(lb<ub){
             int mid=(lb+ub)/2;
-            if(nums[mid]>=target){
-                ans=mid;
-                ub=mid-1;
-            }else{
-                lb=mid+1;
-            }
-        }
-        return ans;
-    }
-
-     int upper(int nums[], int target){
-        int lb=0, ub=nums.length-1;
-        int ans=0;
-        while(lb<=ub){
-            int mid=(lb+ub)/2;
-            if(nums[mid]<=target){
-                ans=mid;
+            if(nums[mid]<target){
                 lb=mid+1;
             }else{
-                ub=mid-1;
+                ub=mid;
             }
         }
-        return ans;
+        return lb;
     }
 }
