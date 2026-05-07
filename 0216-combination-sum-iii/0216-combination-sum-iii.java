c9cba5efbcf65ae338ab006774e59a733fc8bc00
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-07 18:24:21 +0530

    Time: 0 ms (100%), Space: 41.7 MB (99.75%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,21 +1,22 @@
 class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    int nums[];
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res = new ArrayList<>();
-        dfs(k, n, new ArrayList<>(), res, 1);
-        return res;
+        this.nums=new int[]{1,2,3,4,5,6,7,8,9};
+        dfs(k, n, 0, new ArrayList<>());
+        return ans;
     }
 
-    void dfs(int k , int n, List<Integer> curr, List<List<Integer>> res, int start){
-        if(n<0 || curr.size()>k) return;
-        if(n==0 && curr.size()==k){
-            res.add(new ArrayList<>(curr));
+    void dfs(int k, int t, int idx, List<Integer> curr){
+        if(t<0 || k<0) return;
+        if(k==0){
+            if(t==0) ans.add(new ArrayList<>(curr));
             return;
-        }
-
-        for(int i=start;i<=9;i++){
-            if(i>n) break;
-            curr.add(i);
-            dfs(k, n-i,curr, res, i+1);
+        } 
+        // System.out.println(i)
+        for(int i=idx;i<nums.length;i++){
+            curr.add(nums[i]);
+            dfs(k-1, t-nums[i], i+1, curr);
             curr.remove(curr.size()-1);
         }
     }
