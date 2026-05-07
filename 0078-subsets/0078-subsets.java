932337410385a4a4705104ba9116b7890494ddfd
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-07 17:07:04 +0530

    Time: 0 ms (100%), Space: 44.2 MB (38.87%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,22 @@
 class Solution {
+    //1 2 3
+    //inclusion exclusion
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        Arrays.sort(nums);
-        int n= nums.length;
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0;i<(1<<n);i++){
-            List<Integer> curr = new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i&(1<<j))!=0){
-                    curr.add(nums[j]);
-                }
-            }
-            list.add(new ArrayList<>(curr));
+        dfs(nums,0, nums.length, new ArrayList<>());
+        return ans;
+    }
+
+    void dfs(int nums[], int idx, int n, List<Integer> list){
+        if(idx==n){
+            ans.add(new ArrayList<>(list));
+            return;
         }
-        return list;
+        // System.out.println("IN AT"+idx+" "+list);
+        list.add(nums[idx]);
+        dfs(nums, idx+1, n, list);
+        list.remove(list.size()-1);
+        dfs(nums, idx+1, n, list);
+
     }
 }
