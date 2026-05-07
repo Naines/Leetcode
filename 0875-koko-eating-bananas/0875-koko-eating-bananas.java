2b59f5b5d54042893495c4160a13d3a042fc0b1c
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-07 08:59:44 +0530

    Time: 7 ms (79.98%), Space: 47.4 MB (96.08%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,26 +1,38 @@
 class Solution {
+    // 3 6 7 11 , h=8
+    // per hour speed  = (pile[i]+k-1)/k
+    //search space: min(piles), max(piles)
     public int minEatingSpeed(int[] piles, int h) {
-        Arrays.sort(piles);
-        int lb=1;
-        int ub = Arrays.stream(piles).max().getAsInt();
-        int ans =0;
-        //System.out.println(lb+" "+ub);
+        int min=1, max=piles[0];
+        for(int x: piles){
+            min = Math.min(min, x);
+            max= Math.max(max, x);
+        }
+
+        int lb=min, ub=max;
+        int ans=0;
         while(lb<=ub){
-            int m = lb + (ub-lb)/2;
-            long hours = 0;
-            for(int i=0;i<piles.length;i++){
-                hours+= (piles[i]+m-1)/m;
-            }
-            //1,2,3,.....11
-            //5
-            if(hours<=h){
-                ans = m;
-                ub=m-1; 
+            int m = lb+(ub-lb)/2;
+            // System.out.println(m+" "+lb+" "+ub);
+            long reqd=canDo(piles, m);
+            // System.out.println("found: "+m+" "+reqd);
+            if(reqd<=h){
+                ans=m;
+                ub=m-1;
             }else{
                 lb=m+1;
             }
-        }   
+        }
         return ans;
     }
-    
+
+    //find hours taken to clear piles at rate m per hour
+    long canDo(int piles[], int m){
+        long ans=0;
+        for(int i=0;i<piles.length;i++){
+            long hrs = (piles[i]+m-1)/m;
+            ans+=hrs;
+        }
+        return ans;
+    }
 }
