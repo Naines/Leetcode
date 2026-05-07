712e052ba2d2df066311eb85f7b6fa15c5557695
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-07 18:12:12 +0530

    Time: 7 ms (35.74%), Space: 45 MB (87.14%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,22 @@
-class Solution {
+class Solution { 
+    List<List<Integer>> ans=new ArrayList<>();
+    int nums[];
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        this.nums=nums;
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        dfs(nums, 0, target, res, new ArrayList<>());
-        return res;
+        dfs(0, new ArrayList<>(), target);
+        return ans;
     }
-
-    void dfs(int nums[], int idx, int target, List<List<Integer>> res, List<Integer> curr){
-        if(target<0) return;
-        if(target==0){
-            res.add(new ArrayList<>(curr));
+    void dfs(int idx, List<Integer> curr, int t){
+        if(t<0) return;
+        if(t==0){
+            ans.add(new ArrayList<>(curr));
             return;
         }
-
         for(int i=idx;i<nums.length;i++){
-            if (i > idx && nums[i] == nums[i - 1]) continue;
-            if (nums[i] > target) break;
+            if(i>idx && nums[i]==nums[i-1]) continue;
             curr.add(nums[i]);
-            dfs(nums, i+1, target-nums[i], res, curr);
+            dfs(i+1, curr, t-nums[i]);
             curr.remove(curr.size()-1);
         }
     }
