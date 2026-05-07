ecc14624e2186f79aece7aa7aee463616bde8bb2
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-07 12:56:04 +0530

    Time: 76 ms (5.27%), Space: 44.5 MB (73.14%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,34 +1,35 @@
 class Solution {
     //2,3,4,7,11
-    //
+    /**
+    2 3 4 7 11 12
+    arr[i]-(i+1)
+    1 1 1 3  6  6
+    
+     */
     public int findKthPositive(int[] arr, int k) {
-        // int lb = 0, ub=arr.length-1;
-        // while(lb<=ub){
-        //     int m = (lb+ub)/2;
-        //     int missing  = arr[m]-(m+1);
-        //     if(missing>=k){
-        //         ub=m-1;
-        //     }else{
-        //         lb=m+1;
-        //     }
-        // }
-
-        // return lb+k;
-        Set<Integer> set= new HashSet<>();
-        for(int x: arr) set.add(x);
-                // System.out.println(k+arr.length+1+" "+k);
-        int i=1;
-        int n=k+arr.length+1;
-        for(;i<=n;i++){
-            if(!set.contains(i)) k--;
-            // System.out.println(i+" "+k);
-            if(k==0) break;
+        int lb = 0, ub=arr.length-1;
+        while(lb<=ub){
+            int m = (lb+ub)/2;
+            int missing  = arr[m]-(m+1);
+            if(missing>=k){
+                ub=m-1;
+            }else{
+                lb=m+1;
+            }
         }
-        // System.out.println(i+" "+k);
 
-        return k==0?i:-1;
+        return lb+k;
+
+
+        //O(n) solution
+        // Set<Integer> set= new HashSet<>();
+        // for(int x: arr) set.add(x);
+        // int i=1;
+        // int n=k+arr.length+1;
+        // for(;i<=n;i++){
+        //     if(!set.contains(i)) k--;
+        //     if(k==0) break;
+        // }
+        // return k==0?i:-1;
     }
-
-    //2,3,4,7,11
-    //1,5,6,8,9
 }
