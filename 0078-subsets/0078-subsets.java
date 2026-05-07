d270d3a93f4328e6f2a78b13b4049475f7ed1544
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-07 17:46:12 +0530

    Time: 1 ms (88.86%), Space: 44.1 MB (57.04%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -3,20 +3,33 @@ class Solution {
     //inclusion exclusion
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        dfs(nums,0, nums.length, new ArrayList<>());
+        // dfs(nums,0, nums.length, new ArrayList<>());
+        // return ans;
+
+
+        int n= nums.length;
+        for(int i=0;i<(1<<n);i++){
+            List<Integer> curr = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0){
+                    curr.add(nums[j]);
+                }
+            }
+            ans.add(new ArrayList<>(curr));
+        }
         return ans;
     }
 
-    void dfs(int nums[], int idx, int n, List<Integer> list){
-        if(idx==n){
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-        // System.out.println("IN AT"+idx+" "+list);
-        list.add(nums[idx]);
-        dfs(nums, idx+1, n, list);
-        list.remove(list.size()-1);
-        dfs(nums, idx+1, n, list);
+    // void dfs(int nums[], int idx, int n, List<Integer> list){
+    //     if(idx==n){
+    //         ans.add(new ArrayList<>(list));
+    //         return;
+    //     }
+    //     // System.out.println("IN AT"+idx+" "+list);
+    //     list.add(nums[idx]);
+    //     dfs(nums, idx+1, n, list);
+    //     list.remove(list.size()-1);
+    //     dfs(nums, idx+1, n, list);
 
-    }
+    // }
 }
