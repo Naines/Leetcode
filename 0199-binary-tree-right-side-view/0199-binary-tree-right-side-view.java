69ec10cdfd9b1f0edcf3b173b04a951f23ca2f15
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-08 22:23:01 +0530

    Time: 3 ms (1.96%), Space: 43.9 MB (13.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -12,25 +12,51 @@
  *         this.right = right;
  *     }
  * }
+
+
+ right view -> map<row, Integer>, col max value accepted for row
  */
 class Solution {
-    
+    static class Node{
+        TreeNode n;
+        int r,c;
+        Node(TreeNode n, int r, int c){
+            this.n=n;
+            this.c=c;
+            this.r=r;
+        }
+    }
+    List<Integer> ans = new ArrayList<>();
+    Map<Integer, Integer> map=new TreeMap<>();
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList();
-        Queue<TreeNode> q = new LinkedList();
-        q.add(root);
-        
-        while(!q.isEmpty() && root != null) {
-            result.add(q.peek().val);
-            for(int i = q.size(); i > 0; i--) {
-                root = q.poll();
-                if(root.right != null)
-                    q.add(root.right);
-                if(root.left != null)
-                    q.add(root.left);
+        if(root==null) return ans;
+        Queue<Node> q=new LinkedList<>();
+        q.add(new Node(root, 0,0));
+        map.put(0, root.val);        
+        while(!q.isEmpty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                Node t=q.poll();
+            
+                if(t.n.left!=null){
+                    int nr=t.r+1;
+                    int nc=t.c-1;
+                    q.add(new Node(t.n.left, nr, nc));
+                    map.put(nr, t.n.left.val);
+                }
+
+                if(t.n.right!=null){
+                    int nr=t.r+1;
+                    int nc=t.c+1;
+                    q.add(new Node(t.n.right, nr, nc));
+                    map.put(nr, t.n.right.val);
+                }
             }
         }
-        
-        return result;
+        for(Map.Entry<Integer, Integer> e: map.entrySet()){
+            ans.add(e.getValue());
+        }
+        return ans;
     }
+
 }
