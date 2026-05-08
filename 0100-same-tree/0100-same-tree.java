26c4a80cd4603c05687cd33ebccabdfcdf195af7
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-08 20:14:46 +0530

    Time: 0 ms (100%), Space: 42.7 MB (70.22%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,18 +15,11 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        //do inorder transversal and keep checking, in case diff found return false
-        return dfs(p,q);
+        return dfs(p, q);
     }
-
     boolean dfs(TreeNode p, TreeNode q){
-
-        //if leaf node for both
         if(p==null && q==null) return true;
-
-         // If only one tree is null or 
-         //the values are different, they are not identical
-        if(p==null || q==null ||p.val!=q.val) return false;
+        if(p==null || q==null || p.val != q.val) return false;
         return dfs(p.left, q.left) && dfs(p.right, q.right);
     }
 }
