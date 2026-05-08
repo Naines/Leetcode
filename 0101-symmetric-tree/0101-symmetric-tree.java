adec2e123b31a89ca82be3082e9249955b4fa43b
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-09 01:37:54 +0530

    Time: 0 ms (100%), Space: 43.4 MB (84.18%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -19,9 +19,9 @@ class Solution {
         return dfs(root.left, root.right);
     }
     boolean dfs(TreeNode p, TreeNode q){
-         if (p == null || q == null)
-            return p == q;
-       
-        return (p.val == q.val) && dfs(p.left, q.right) && dfs(p.right, q.left);
+       if(p==null && q==null) return true;
+       if(p==null || q==null) return false;
+       return p.val==q.val && dfs(p.left, q.right) && dfs(p.right, q.left);
+
     }
 }
