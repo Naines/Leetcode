5f3ed4f3825a5d113ada5e42ea2b03d3d10fdd06
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-09 14:56:53 +0530

    Time: 1 ms (99.5%), Space: 45.2 MB (48.15%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,45 +13,99 @@
  *     }
  * }
  */
+
+ /**
+ BFS- empty nodes
+ take -1 nonempty nodes in q
+ cf
+
+ 1 0
+2 1
+4 2
+4 3
+8 4
+16 5
+32 6
+0 7
+  */
 class Solution {
-    static class Node{
-        TreeNode n;
-        int i;
-        Node(TreeNode n, int i){
-            this.n=n;
-            this.i=i;
-        }
+    class Node {
+        TreeNode node;
+        int idx;
+        Node(TreeNode node, int idx){
+            this.node = node;
+            this.idx = idx;
+        }  
     }
     public int widthOfBinaryTree(TreeNode root) {
-        if(root.right ==null && root.left==null) return 1;
-        Queue<Node> q=new LinkedList<>();
+        Queue<Node> q = new LinkedList<>();
         q.add(new Node(root,0));
-        int ans =1;
-        while(q.size()>0){
-            int s=q.size();
-            int idmin = Integer.MAX_VALUE, idmax=Integer.MIN_VALUE;
-            System.out.println(s);
-            for(int i=0;i<s;i++){
-
-                Node t = q.poll();
-                
-                if(t.n.left!=null){
-                    int id = (t.i*2) +1;
-                    q.add(new Node(t.n.left, id));
-                    idmin=Math.min(idmin, id);
-                    idmax = Math.max(idmax, id);
+        int max = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            int start = 0, end = 0;
+            for(int i=0; i<size; i++){
+                Node p = q.poll();
+                int index = p.idx; 
+                if(i==0) start = index; //start and end index for each level
+                if(i==size-1) end = index;
+                if(p.node.left!=null){
+                    q.add(new Node(p.node.left, 2*p.idx+1));
                 }
-                if(t.n.right!=null){
-                     int id = (t.i*2) +2;
-                    q.add(new Node(t.n.right, id ));
-                    idmin=Math.min(idmin, id);
-                    idmax = Math.max(idmax, id);
+                
+                if(p.node.right!=null){
+                    q.add(new Node(p.node.right, 2*p.idx+2));
                 }
             }
-            System.out.println(idmin+" "+idmax);
-            if(idmin!=Integer.MAX_VALUE || idmax!=Integer.MIN_VALUE)
-            ans = Math.max(ans , idmax-idmin+1);
+            max = Math.max(max, end - start + 1);
         }
-        return ans;
+        return max; 
     }
+
+
+    //TLE
+    //  public int widthOfBinaryTree(TreeNode root) {
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     q.add(root);
+    //     int r=-1,ans=0;
+    //     while(!q.isEmpty()){
+    //         int s=q.size();
+    //         int wf=0,w=0;
+    //         boolean start=false;
+    //         r++;
+    //         for(int i=0;i<s;i++){
+    //             TreeNode t=q.poll();
+
+    //             if(t.val!=-1){
+    //                 w++;
+    //                 if(start) w+=wf;
+    //                 wf=0;
+    //                 start=true;
+    //             }else{
+    //                 wf++;
+    //             }
+
+    //             if(t.left!=null){
+    //                 q.add(t.left);
+    //             }else{
+    //                 TreeNode emptyNode=new TreeNode(-1);
+    //                 t.left=emptyNode;
+    //                 q.add(emptyNode);
+    //             }
+
+    //             if(t.right!=null){
+    //                 q.add(t.right);
+    //             }else{
+    //                 TreeNode emptyNode=new TreeNode(-1);
+    //                 t.right=emptyNode;
+    //                 q.add(emptyNode);
+    //             }
+    //             // System.out.println(t.val+" "+w);
+    //         }
+    //         // System.out.println(w+" "+r);
+    //         if(wf==(1<<r)) break;
+    //         ans=Math.max(ans, w);
+    //     }
+    //     return ans;
+    // }
 }
