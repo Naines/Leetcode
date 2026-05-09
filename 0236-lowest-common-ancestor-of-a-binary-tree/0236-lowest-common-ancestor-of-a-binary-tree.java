4180263e1c04cd2b478edd7b29377634ac66a68b
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-09 09:48:08 +0530

    Time: 7 ms (98.83%), Space: 46.4 MB (84.28%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -14,9 +14,8 @@ class Solution {
     TreeNode lca(TreeNode root,TreeNode p, TreeNode q){
         if(root==null) return null;
         TreeNode l=lca(root.left, p, q), r=lca(root.right,p,q);
-        if(root==p || root==q) return root;
-        if(l==null) return r;
-        if(r==null) return l;
-        return root;
+        if(root==p || root==q) return root; //case1: root is one of nodes
+        if(l!=null && r!=null) return root; //case2: root children has p and q
+        return l!=null?l:r; //case 3,4: one of child have p||q or both absent  
     }
 }
