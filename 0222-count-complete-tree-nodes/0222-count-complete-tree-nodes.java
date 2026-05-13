fd11807b7eeba981460bfb1f712b715872030e6c
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-13 12:57:41 +0530

    Time: 0 ms (100%), Space: 49.6 MB (12.67%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -18,14 +18,16 @@ class Solution {
         if(root==null) return -1;
         return height(root.left)+1;
     }
+
+    // goal is to reach last node of last row
     public int countNodes(TreeNode root) {
         int ans =0, h=height(root);
         while(root!=null){
             if(height(root.right)==h-1){
-                ans+=(1<<h);
+                ans+=(1<<h); //nodes in left ST
                 root=root.right;
             }else{
-                ans+=(1<<h-1);
+                ans+=(1<<h-1); //nodes in right ST
                 root=root.left;
             }
             h--;
