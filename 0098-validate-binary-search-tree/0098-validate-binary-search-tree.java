61153f5d378576cbb24fd3deeacb3abd70442960
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-13 16:05:46 +0530

    Time: 2 ms (12.72%), Space: 45.3 MB (32.95%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -18,18 +18,36 @@
  CASE: [-2147483648,-2147483648]
   */
 class Solution {
-    long min = (long)Integer.MIN_VALUE-100, max=(long)Integer.MAX_VALUE+100;
     public boolean isValidBST(TreeNode root) {
-        // System.out.println(min+" "+max);
-        return dfs(root, min, max);
-    }
-    boolean dfs(TreeNode root, long min, long max){
         if(root==null) return true;
-        boolean l= dfs(root.left, min ,(long)root.val-1);
-        boolean r = dfs(root.right, (long)root.val+1, max);
-        // System.out.println("IN AT "+root.val+" "+min+" "+max+" "+l+" "+r);
-        if(root.val<min || root.val>max) return false;
-        if(l==false || r==false) return false;
+        Stack<TreeNode> stk=new Stack<>();
+        TreeNode pre=null;
+        while(root!=null || !stk.isEmpty()){
+            while(root!=null){
+                stk.push(root);
+                root=root.left;
+            }
+            root=stk.pop();
+            if(pre!=null && root.val<=pre.val) return false;
+            pre=root;
+            root=root.right;
+        }
         return true;
     }
+  
+    //METHOD 1: 
+    //  long min = (long)Integer.MIN_VALUE-100, max=(long)Integer.MAX_VALUE+100;
+    // public boolean isValidBST(TreeNode root) {
+    //     // System.out.println(min+" "+max);
+    //     return dfs(root, min, max);
+    // }
+    // boolean dfs(TreeNode root, long min, long max){
+    //     if(root==null) return true;
+    //     boolean l= dfs(root.left, min ,(long)root.val-1);
+    //     boolean r = dfs(root.right, (long)root.val+1, max);
+    //     // System.out.println("IN AT "+root.val+" "+min+" "+max+" "+l+" "+r);
+    //     if(root.val<min || root.val>max) return false;
+    //     if(l==false || r==false) return false;
+    //     return true;
+    // }
 }
