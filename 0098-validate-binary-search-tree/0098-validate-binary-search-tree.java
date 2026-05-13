e84c3a7c854dd4ee698977ba6621a95c4060cdc6
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-13 16:01:21 +0530

    Time: 0 ms (100%), Space: 45 MB (63.64%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,21 +13,23 @@
  *     }
  * }
  */
+ /**
+ CASE: [2,2,2] 
+ CASE: [-2147483648,-2147483648]
+  */
 class Solution {
+    long min = (long)Integer.MIN_VALUE-100, max=(long)Integer.MAX_VALUE+100;
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode pre = null;
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            if(pre != null && root.val <= pre.val) return false;
-            pre = root;
-            root = root.right;
-        }
+        // System.out.println(min+" "+max);
+        return dfs(root, min, max);
+    }
+    boolean dfs(TreeNode root, long min, long max){
+        if(root==null) return true;
+        boolean l= dfs(root.left, min ,(long)root.val-1);
+        boolean r = dfs(root.right, (long)root.val+1, max);
+        // System.out.println("IN AT "+root.val+" "+min+" "+max+" "+l+" "+r);
+        if(root.val<min || root.val>max) return false;
+        if(l==false || r==false) return false;
         return true;
     }
 }
