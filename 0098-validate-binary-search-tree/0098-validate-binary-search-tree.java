5cd1223084d0daafe8d2be4eb4fae2ed9194aa27
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-13 16:09:04 +0530

    Time: 0 ms (100%), Space: 45.2 MB (48.58%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -18,20 +18,13 @@
  CASE: [-2147483648,-2147483648]
   */
 class Solution {
+    TreeNode pre=null;
     public boolean isValidBST(TreeNode root) {
         if(root==null) return true;
-        Stack<TreeNode> stk=new Stack<>();
-        TreeNode pre=null;
-        while(root!=null || !stk.isEmpty()){
-            while(root!=null){
-                stk.push(root);
-                root=root.left;
-            }
-            root=stk.pop();
-            if(pre!=null && root.val<=pre.val) return false;
-            pre=root;
-            root=root.right;
-        }
+        if(!isValidBST(root.left)) return false;
+        if(pre!=null && pre.val>=root.val) return false;
+        pre=root;
+        if(!isValidBST(root.right)) return false;
         return true;
     }
   
@@ -48,6 +41,24 @@ class Solution {
     //     // System.out.println("IN AT "+root.val+" "+min+" "+max+" "+l+" "+r);
     //     if(root.val<min || root.val>max) return false;
     //     if(l==false || r==false) return false;
+    //     return true;
+    // }
+
+    //METHOD 2
+    //  public boolean isValidBST(TreeNode root) {
+    //     if(root==null) return true;
+    //     Stack<TreeNode> stk=new Stack<>();
+    //     TreeNode pre=null;
+    //     while(root!=null || !stk.isEmpty()){
+    //         while(root!=null){
+    //             stk.push(root);
+    //             root=root.left;
+    //         }
+    //         root=stk.pop();
+    //         if(pre!=null && root.val<=pre.val) return false;
+    //         pre=root;
+    //         root=root.right;
+    //     }
     //     return true;
     // }
 }
