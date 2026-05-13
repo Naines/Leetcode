fd83a5d7e62ee4110192ba5d777b9b9ae85bfad9
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-13 17:50:29 +0530

    Time: 1 ms (100%), Space: 46.2 MB (93.18%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -31,8 +31,8 @@ class Solution {
             if(list.get(i).val<list.get(i-1).val){
                 //get other unordered ele
                 int j=i-1;
-                while(j>=0 && list.get(j).val>list.get(i).val) j--;
-                
+                while(j>=0 && list.get(j).val>list.get(i).val)
+                 j--;
                 //replace i and j+1 as j is smaller than i
                 int temp=list.get(i).val;
                 list.get(i).val=list.get(j+1).val;
@@ -43,21 +43,21 @@ class Solution {
     }
     */
     
-    TreeNode prev=new TreeNode(Integer.MIN_VALUE), first=null,second=null;
+    TreeNode prev=new TreeNode(Integer.MIN_VALUE),
+        first=null,
+        second=null;
     public void recoverTree(TreeNode root){
         transverse(root);
         int temp=first.val;
         first.val=second.val;
         second.val=temp;
     }
-    public void transverse(TreeNode curr){
-        if(curr==null) return;
-        transverse(curr.left);
-        
-        if(first==null && prev.val>curr.val)    first=prev;
-        if(first!=null && prev.val>curr.val)    second=curr;
-        
-        prev=curr;
-        transverse(curr.right);
+    public void transverse(TreeNode node){
+        if(node==null) return;
+        transverse(node.left);
+        if(first==null && prev.val>node.val) first=prev;
+        if(first!=null && prev.val>node.val) second=node;
+        prev=node;
+        transverse(node.right);
     }
 }
