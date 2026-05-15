a926ea2e3db6ed84f1cbcf3df6816a35309bb351
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-15 22:59:42 +0530

    Time: 50 ms (31.4%), Space: 47.7 MB (37.63%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,18 +1,24 @@
 class Solution {
-    //dp[i][j] = states if first j char matches first i char
-    //if ? -> dp[i][j] = dp[i-1][j-1] 
-    //if * -> dp[i][j] = dp[i-1][j] || dp[i][j-1]
-    //else char c - macth > dp[i][j] = dp[i-1][j-1]
-    //else dont match -> dp[i][j]=false 
+
+    // dp[i][j] = states if first j char matches first i char
+    // if ? -> dp[i][j] = dp[i-1][j-1] 
+    // if * -> dp[i][j] = dp[i-1][j] || dp[i][j-1] 
+    // (match no char, move t behind or matches none, move s behind, keep t)
+    // else char c - macth > dp[i][j] = dp[i-1][j-1]
+    // else dont match -> dp[i][j]=false 
     public boolean isMatch(String s, String p) {
         int m=s.length(), n=p.length();
         boolean dp[][]=new boolean[m+1][n+1];
         dp[0][0]=true; //"",""
+
+
+         // empty text matches with string of '*'s
         for(int i=1;i<=n;i++){
             if(p.charAt(i-1)=='*') {
                 dp[0][i]=dp[0][i-1];
             }
         }
+
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
