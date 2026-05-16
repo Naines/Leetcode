421686d2279e1d53782e4e62134c3fe4473ab610
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-16 14:50:44 +0530

    Time: 58 ms (47.04%), Space: 47.1 MB (9.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,10 +1,22 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return dfs(nums, 0, target);
+        if (nums == null || nums.length == 0) return 0;
+        Map<Integer, Map<Integer, Integer>> mem = new HashMap<>();
+        return process(nums, target, 0, mem);
     }
 
-    int dfs(int a[], int i, int t){
-        if(i==a.length) return t==0?1:0;
-        return dfs(a, i+1, t-a[i])+dfs(a, i+1, t+a[i]);
+    private int process(int[] arr, int target, int i, Map<Integer, Map<Integer, Integer>> mem) {
+        if (i == arr.length) {
+            if (target == 0) return 1;
+            return 0;
+        }
+        if (mem.containsKey(i) && mem.get(i).containsKey(target)) {
+            return mem.get(i).get(target);
+        }
+        int res1 = process(arr, target - arr[i], i + 1, mem);
+        int res2 = process(arr, target + arr[i], i + 1, mem);
+        int res = res1 + res2;
+        mem.computeIfAbsent(i, k -> new HashMap<>()).put(target, res);
+        return res;
     }
 }
