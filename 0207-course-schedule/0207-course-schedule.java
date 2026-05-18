6c7342d71c5653fca2053de525392f4eda4869ec
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-19 02:35:32 +0530

    Time: 7 ms (59.24%), Space: 47.5 MB (12.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,41 +1,27 @@
 class Solution {
 
-    //[a,b]
-    //b->a
-    boolean vis[], in[];
-    public boolean canFinish(int n, int[][] arr) {
-        this.vis=new boolean[n];
-        this.in=new boolean[n];
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-
-        for(int i=0;i<arr.length;i++){
-            adj.get(arr[i][1]).add(arr[i][0]);
+    //[a,b] : b->a
+    public boolean canFinish(int n, int[][] pre) {
+        int in[]=new int[n];
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++) graph.add(new ArrayList<>());
+        for(int i=0;i<pre.length;i++){
+            graph.get(pre[i][1]).add(pre[i][0]);
+            in[pre[i][0]]++;
         }
-        
-        for(int i=0;i<n;i++){
-            if(vis[i]) continue;
-            if(dfs(adj, n, i)) return false;
-        }
-        return true;
-    }
+        for(int i=0;i<n;i++) if(in[i]==0) q.add(i);
 
-    boolean dfs(List<List<Integer>> adj, int n, int u){
-        vis[u]=true;
-        in[u]=true;
-        // System.out.println("in at"+u);
-        // System.out.println(Arrays.toString(vis));
-        // System.out.println(Arrays.toString(in));
-        for(int v: adj.get(u)){
-            if(!vis[v]){
-                if(dfs(adj, n, v)) 
-                    return true;
-            }else if(in[v]){
-                return true;
+        List<Integer> top=new ArrayList<>();
+        //count==top.size()
+        while(!q.isEmpty()){
+            int t=q.poll();
+            top.add(t);
+            for(int v: graph.get(t)){
+                in[v]--;
+                if(in[v]==0) q.add(v);
             }
         }
-        in[u]=false;
-        return false;
+        return top.size()==n;
     }
-
 }
