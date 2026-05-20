636f79c9a474eeca67e978b19cd153cfe96c9552
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-21 03:15:15 +0530

    Time: 4 ms (43.14%), Space: 52.4 MB (46.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,32 @@
 class Solution {
-    static int dirs[][]={{1,0},{0,1},{-1,0},{0,-1}};
-    void dfs(char grid[][], boolean vis[][], int r, int c){
-        
-        boolean isNotSafe = r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]
-        =='0' || vis[r][c]==true;
-        if(isNotSafe) return;
-        // System.out.println("IN AT "+r+" "+c+" "+grid[r][c]);
-        vis[r][c]=true;
-        dfs(grid, vis, r+1, c);
-        dfs(grid, vis, r-1, c);
-        dfs(grid, vis, r, c+1);
-        dfs(grid, vis, r, c-1);
-    }
-       
+    char grid[][];
     public int numIslands(char[][] grid) {
-        int h = grid.length, w=grid[0].length;
-        boolean vis[][]=new boolean[h][w];
         int ans =0;
-        for(int i=0;i<h;i++){
-            for(int j=0;j<w;j++){
-                if(grid[i][j]=='1' && vis[i][j]==false){
-                    dfs(grid, vis, i, j);
+        this.grid=grid;
+        boolean vis[][]=new boolean[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    // System.out.println("start"+i+" "+j);
+                    dfs(i, j, vis);
                     ans++;
                 }
+                
+
             }
         }
         return ans;
+    }
+    int dir[][]=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+    void dfs(int x, int y, boolean vis[][]){
+        if(x<0 || y<0 || x>=vis.length|| y>=vis[0].length|| vis[x][y] || grid[x][y]=='0'){
+            return;
+        }
+        vis[x][y]=true;
+        // System.out.println("in at "+x+" "+y+" "+grid[x][y]);
+        for(int i=0;i<4;i++){
+            int xx=x+dir[i][0], yy=y+dir[i][1];
+            dfs(xx, yy, vis);
+        }
     }
 }
