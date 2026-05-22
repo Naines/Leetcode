bcf616793ad0ff5180f9cc38dfc5e37799c72b51
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-05-22 22:04:00 +0530

    Time: 1188 ms (57.06%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -5,4 +5,11 @@
 select eu.unique_id, e.name
 from employees e
 left join EmployeeUNI eu
-on e.id = eu.id;
+on e.id = eu.id
+order by eu.unique_id;
+
+-- SELECT   eu.unique_id, e.name  
+-- FROM  Employees e
+-- LEFT JOIN EmployeeUNI eu 
+-- ON e.id = eu.id
+-- ORDER BY eu.unique_id ;
