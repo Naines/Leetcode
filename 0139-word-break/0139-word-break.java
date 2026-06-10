273e1a14c4c0f0c425c44c80f1c8d053cede0e60
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-06-10 19:28:23 +0530

    Time: 8 ms (51.31%), Space: 46.3 MB (34.42%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,31 @@
 class Solution {
-    
+    Set<String> set = new HashSet<>();
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>();
+        for(String str: wordDict){
+            set.add(str);
+        }
         Map<String, Boolean> map = new HashMap<>();
-        return dfs(s, wordDict, map);
+        // found[0] = true;
+        dfs(s, map);
+        if(!map.containsKey(s)) return false;
+        return map.get(s);
     }
 
-    boolean dfs(String s, List<String> wordDict, Map<String, Boolean> map){
+    boolean dfs(String s, Map<String, Boolean> map){
         if(s.length()==0) return true;
         if(map.containsKey(s)) return map.get(s);
-
+        // System.out.println(s); 
         for(int i=0;i<s.length();i++){
-            String left = s.substring(0,i+1);
-            if(wordDict.contains(left)){
-                String right = s.substring(i+1);
-                boolean res = dfs(right, wordDict, map);
-                if(res){
-                    map.put(right, true);
-                    return true;
-                }
+            String left = s.substring(0, i+1);
+            if(set.contains(left) && dfs(s.substring(i+1),map)){
+                map.put(s, true);
+                return true;
             }
+                
         }
         map.put(s, false);
         return false;
     }
+
+   
 }
