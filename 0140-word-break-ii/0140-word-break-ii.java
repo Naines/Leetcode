11baab3677e833881e61521049f74ba798defff9
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-06-10 19:56:11 +0530

    Time: 0 ms (100%), Space: 43 MB (84.43%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,26 +1,35 @@
 class Solution {
-    ArrayList<String> list=new ArrayList<>();
-    public List<String> wordBreak(String s, List<String> wordDict) 
-    {
-        solve(s, wordDict, "");
-        return list;
-    }
+    /**
+        generate all possible strings 
+        cat -> sand -> dog
+        left + " "+right
+
+        cats -> and -> dog 
     
-    void solve(String str, List<String> dict, String ans)
-    {
-        if(str.length()==0)
-        {
-            list.add(ans.substring(0,ans.length()-1)); 
+     */
+
+    List<String> ans;
+    Set<String> set=new HashSet<>();
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        ans = new ArrayList<>();
+        for(String str: wordDict) set.add(str);
+        dfs(s,new StringBuilder());
+        return ans;
+    }
+
+    void dfs(String s, StringBuilder sb){
+        if (s.length() == 0) {
+            ans.add(sb.substring(0, sb.length() - 1));
             return;
         }
-        
-        for(int i=0;i<str.length();i++)
-        {
-            String left=str.substring(0,i+1);
-            if(dict.contains(left))
-            {
-                String right=str.substring(i+1);
-                solve(right, dict, ans+left+" ");
+
+        for (int i=0; i<s.length(); i++) {
+            String left = s.substring(0, i+1);
+            if (set.contains(left)) {
+                int lenBefore = sb.length();
+                sb.append(left).append(" ");
+                dfs(s.substring(i + 1), sb);
+                sb.setLength(lenBefore);
             }
         }
     }
