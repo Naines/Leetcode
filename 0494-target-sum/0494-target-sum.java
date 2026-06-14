89c0fe5c24395e36d800fb35a693bf0a87c41b96
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-06-14 15:22:22 +0530

    Time: 130 ms (34.71%), Space: 48 MB (6.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,26 @@
 class Solution {
     int nums[];
     int count=0;
+    Map<String, Integer> map = new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
        this.nums = nums;
         dfs(target, 0);
-        return count;
+        return map.get(target+"-"+0);
     }
 
-    void dfs(int target, int idx){
+    int dfs(int target, int idx){
+        String key = target+"-"+idx;
+        if(map.containsKey(key)) return map.get(key);
+
         if(idx==nums.length){
-            if(target==0){
-                count++;
-            }
-            return;
+            map.put(key, target==0?1:0);           
+            return map.get(key);
         }
 
-        dfs(target+nums[idx], idx+1);
+
+        int ways = dfs(target+nums[idx], idx+1)+
         dfs(target-nums[idx], idx+1);
+        map.put(key, ways);
+        return ways;
     }
 }
