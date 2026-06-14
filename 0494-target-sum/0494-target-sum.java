7427bac4213980abc59d678e6d4add66abd04afa
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-06-14 15:07:45 +0530

    Time: 621 ms (23.88%), Space: 42.9 MB (93.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,21 @@
 class Solution {
+    int nums[];
+    int count=0;
     public int findTargetSumWays(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return 0;
-        Map<Integer, Map<Integer, Integer>> mem = new HashMap<>();
-        return process(nums, target, 0, mem);
+       this.nums = nums;
+        dfs(target, 0);
+        return count;
     }
 
-    private int process(int[] arr, int target, int i, Map<Integer, Map<Integer, Integer>> mem) {
-        if (i == arr.length) {
-            if (target == 0) return 1;
-            return 0;
+    void dfs(int target, int idx){
+        if(idx==nums.length){
+            if(target==0){
+                count++;
+            }
+            return;
         }
-        if (mem.containsKey(i) && mem.get(i).containsKey(target)) {
-            return mem.get(i).get(target);
-        }
-        int res1 = process(arr, target - arr[i], i + 1, mem);
-        int res2 = process(arr, target + arr[i], i + 1, mem);
-        int res = res1 + res2;
-        mem.computeIfAbsent(i, k -> new HashMap<>()).put(target, res);
-        return res;
+
+        dfs(target+nums[idx], idx+1);
+        dfs(target-nums[idx], idx+1);
     }
 }
