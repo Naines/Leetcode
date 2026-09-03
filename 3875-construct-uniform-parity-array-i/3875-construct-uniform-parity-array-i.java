4d519afb9bdcb1de7475fb96bd6a293b252ab030
Nainesh Goel <68371428+Naines@users.noreply.github.com>
2026-09-04 01:54:51 +0530

    Time: 0 ms (100%), Space: 45.1 MB (64.89%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    //all even or all odd -> copy
+    //if either e or o, 
+    //e-o = o, o - e = o
+    //either all odd or all even, if not then convert all to odd
+    public boolean uniformArray(int[] nums1) {
+        // int n = nums1.length;
+        // int nums2[]= new int[n];
+        // boolean even = false, odd= false;
+        // for(int i=0;i<n;i++) 
+        //     if(nums[i]%2!=0){
+        //         x = nums[i];
+        //         break;
+        //     }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2!=0){
+        //         nums2[i] = nums1[i];
+        //     }else{
+        //         nums2[i]-=x;
+        //     }
+        // }
+        return true;
+    }
+}
